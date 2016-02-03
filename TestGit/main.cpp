9a243a1740b959dcb4540194299f35e0d7e38863
winsarrocena <wins.arrocena@gmail.com>
2016-02-03 23:43:53 +0800

    First commit
    This is a sample project

---
+++ TestGit/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "This is a qt project.";
+
+    return a.exec();
+}
+
